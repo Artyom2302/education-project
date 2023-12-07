e1086c9bc11c2e69fc99280479cfbd506c60bc3d
Artyom2302 <89930657+Artyom2302@users.noreply.github.com>
2023-12-07 19:25:38 +0300

    инициализация

---
+++ src/main/java/service/MainClass.java
@@ -0,0 +1,33 @@
+package service;
+
+import dto.House;
+
+public class MainClass {
+    /**
+     * Многострочный комментарий
+     * */
+    public static void main(String[] args) {
+        int numberOfDonuts = 1;
+        //Однострочный комментарий
+        /*Однострочный комментарий*/
+
+        House woodHouse = new House();
+        woodHouse.setMaterial("дерево");
+        updateMaterial(woodHouse);
+        System.out.println(woodHouse);
+
+        int firstNumber = 1;
+        updateNum(firstNumber);
+        System.out.println(firstNumber);
+    }
+
+    public static void updateMaterial(House house){
+        house.setMaterial("кирпич");
+        house = new House();
+        house.setMaterial("пластик");
+    }
+    public static void updateNum(int number){
+        number = 42;
+    }
+}
+
